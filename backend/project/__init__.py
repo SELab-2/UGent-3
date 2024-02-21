71830fc422a2b056417a4cec6701afe878152ac0
cmekeirl <cedric.mekeirle@ugent.be>
2024-02-21 14:20:25 +0100

    fixed db_uri

--- backend/project/__init__.py
+++ backend/project/__init__.py
@@ -2,12 +2,14 @@
 This file is the base of the Flask API. It contains the basic structure of the API.    
 """
 
+from os import getenv
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from .endpoints.index import index_bp
 
 db = SQLAlchemy()
-
+load_dotenv()
 
 def create_app():
     """
@@ -16,7 +18,7 @@ def create_app():
         Flask -- A Flask application instance
     """
     app = Flask(__name__)
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DB_HOST')
     app.register_blueprint(index_bp)
     db.init_app(app)
 
