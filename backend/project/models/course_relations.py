fbee136ad13427769ca6791578344c2387c17e0e
cmekeirl <cedric.mekeirle@ugent.be>
2024-02-21 14:02:27 +0100

    flask_sqlalchemy in requirements

--- backend/project/models/course_relations.py
+++ backend/project/models/course_relations.py
@@ -1,7 +1,7 @@
 """Models for relation between users and courses"""
 # pylint: disable=too-few-public-methods
 
-from sqlalchemy import INTEGER, Column, ForeignKey, PrimaryKeyConstraint, String
+from sqlalchemy import Integer, Column, ForeignKey, PrimaryKeyConstraint, String
 from project import db
 
 class BaseCourseRelation(db.Model):
@@ -9,7 +9,7 @@ class BaseCourseRelation(db.Model):
 
     __abstract__ = True
 
-    course_id = Column(INTEGER, ForeignKey('Courses.course_id'), nullable=False)
+    course_id = Column(Integer, ForeignKey('Courses.course_id'), nullable=False)
     uid = Column(String(255), ForeignKey("Users.uid"), nullable=False)
     __table_args__ = PrimaryKeyConstraint("course_id", "uid")
 
--- backend/project/models/courses.py
+++ backend/project/models/courses.py
@@ -1,6 +1,6 @@
 """The Courses model"""
 # pylint: disable=too-few-public-methods
-from sqlalchemy import INTEGER, Column, ForeignKey, String
+from sqlalchemy import Integer, Column, ForeignKey, String
 from project import db
 
 
@@ -8,7 +8,7 @@ class Courses(db.Model):
     """Course model"""
 
     __tablename__ = "courses"
-    course_id = Column(INTEGER, primary_key=True)
+    course_id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
     ufora_id = Column(String(50), nullable=True)
     teacher = Column(String(255), ForeignKey("Users.uid"), nullable=False)
