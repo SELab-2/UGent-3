42c19dbc4e3b67e41550ac97ee1023badacb589d
Jarne Clauw <67628242+JarneClauw@users.noreply.github.com>
2024-02-24 14:03:31 +0100

    #15 - HTTP methods (skeleton)

--- backend/project/__init__.py
+++ backend/project/__init__.py
@@ -5,6 +5,7 @@ This file is the base of the Flask API. It contains the basic structure of the A
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from .endpoints.index import index_bp
+from .endpoints.submissions import submissions_bp
 
 db = SQLAlchemy()
 
@@ -17,6 +18,7 @@ def create_app():
 
     app = Flask(__name__)
     app.register_blueprint(index_bp)
+    app.register_blueprint(submissions_bp)
 
     return app
 
---
+++ backend/project/endpoints/submissions.py
@@ -0,0 +1,70 @@
+"""Submission API endpoint"""
+
+from flask import Blueprint
+from flask_restful import Resource
+
+submissions_bp = Blueprint("submissions", __name__)
+
+class Submissions(Resource):
+    """API endpoint for the submissions"""
+
+    def get(self, uid: int, pid: int) -> dict[str, int]:
+        """Get all the submissions from a user for a project
+
+        Args:
+            uid (int): User ID
+            pid (int): Project ID
+
+        Returns:
+            dict[str, int]: The list of submission URLs
+        """
+        return {"uid": uid, "pid": pid}
+
+    def post(self, uid: int, pid: int) -> dict[str, int]:
+        """Post a new submission to a project
+
+        Args:
+            uid (int): User ID
+            pid (int): Project ID
+
+        Returns:
+            dict[str, int]: The URL to the submission
+        """
+        return {"uid": uid, "pid": pid}
+
+submissions_bp.add_url_rule(
+    "/submissions/<int:uid>/<int:pid>",
+    view_func=Submissions.as_view("submissions"))
+
+class Submission(Resource):
+    """API endpoint for the submission"""
+
+    def get(self, uid: int, pid: int, sid: int) -> dict[str, int]:
+        """Get the submission given an submission ID
+
+        Args:
+            uid (int): User ID
+            pid (int): Project ID
+            sid (int): Submission ID
+
+        Returns:
+            dict[str, int]: The submission
+        """
+        return {"uid": uid, "pid": pid, "sid": sid}
+
+    def delete(self, uid: int, pid: int, sid: int) -> dict[str, int]:
+        """Delete a submission given an submission ID
+
+        Args:
+            uid (int): User ID
+            pid (int): Project ID
+            sid (int): Submission ID
+
+        Returns:
+            dict[str, int]: Empty
+        """
+        return {"uid": uid, "pid": pid, "sid": sid}
+
+submissions_bp.add_url_rule(
+    "/submissions/<int:uid>/<int:pid>/<int:sid>",
+    view_func=Submission.as_view("submission"))
