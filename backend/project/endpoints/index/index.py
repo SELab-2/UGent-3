d18d982c4d1b7a6115030eceeb2349173edb77fe
Aron Buzogany <aronsaps@gmail.com>
2024-02-24 11:39:22 +0100

    linting: multi-line function docs ender on his own

--- backend/project/endpoints/index/index.py
+++ backend/project/endpoints/index/index.py
@@ -10,8 +10,10 @@ class Index(Resource):
     """Api endpoint for the / route"""
 
     def get(self):
-        """Example of an api endpoint function that will respond to get requests made to /
-        return a json data structure with key Message and value Hello World!"""
+        """
+        Example of an api endpoint function that will respond to get requests made to
+        return a json data structure with key Message and value Hello World!
+        """
         dir_path = os.path.dirname(os.path.realpath(__file__))
         return send_from_directory(dir_path, "OpenAPI_Object.json")
 
