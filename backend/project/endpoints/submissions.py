5ff71c08500e433e1df20b0629e0dd6d13fa5a71
Jarne Clauw <67628242+JarneClauw@users.noreply.github.com>
2024-03-01 10:38:38 +0100

    #15 - Updating the endpoint to use query parameters

--- backend/project/endpoints/submissions.py
+++ backend/project/endpoints/submissions.py
@@ -1,6 +1,7 @@
 """Submission API endpoint"""
 
 from datetime import datetime
+from os import getenv
 from flask import Blueprint, request
 from flask_restful import Resource
 from sqlalchemy import exc
@@ -14,12 +15,8 @@ submissions_bp = Blueprint("submissions", __name__)
 class Submissions(Resource):
     """API endpoint for the submissions"""
 
-    def get(self, uid: str, pid: int) -> dict[str, any]:
+    def get(self) -> dict[str, any]:
         """Get all the submissions from a user for a project
-
-        Args:
-            uid (str): User ID
-            pid (int): Project ID
 
         Returns:
             dict[str, any]: The list of submission URLs
@@ -27,30 +24,29 @@ class Submissions(Resource):
 
         try:
             with db.session() as session:
-                # Check user
-                user = session.get(m_users, uid)
-                if user is None:
-                    return {"message": f"User {uid} not found"}, 404
+                query = session.query(m_submissions)
 
-                # Check project
-                project = session.get(m_projects, pid)
-                if project is None:
-                    return {"message": f"Project {pid} not found"}, 404
+                # Filter by uid
+                uid = request.args.get("uid")
+                if (uid is not None) and (session.get(m_users, uid) is not None):
+                    query.filter_by(uid=uid)
+
+                # Filter by project_id
+                project_id = request.args.get("project_id")
+                if (project_id is not None) and (session.get(m_projects, project_id) is not None):
+                    query.filter_by(project_id=project_id)
 
                 # Get the submissions
-                submissions = session.query(m_submissions).filter_by(uid=uid, project_id=pid).all()
-                submissions_urls = [f"/submissions/{s.submission_id}" for s in submissions]
+                submissions = query.all()
+                submissions_urls = [
+                    f"{getenv('HOSTNAME')}/submissions/{s.submission_id}" for s in submissions
+                ]
                 return {"submissions": submissions_urls}
         except exc.SQLAlchemyError:
-            return {"message": f"An error occurred while fetching the submissions "
-                f"from user {uid} for project {pid}"}, 500
+            return {"message": "An error occurred while fetching the submissions"}, 500
 
-    def post(self, uid: str, pid: int) -> dict[str, any]:
+    def post(self) -> dict[str, any]:
         """Post a new submission to a project
-
-        Args:
-            uid (str): User ID
-            pid (int): Project ID
 
         Returns:
             dict[str, any]: The URL to the submission
@@ -61,16 +57,16 @@ class Submissions(Resource):
                 submission = m_submissions()
 
                 # User
-                user = session.get(m_users, uid)
-                if user is None:
+                uid = request.form.get("uid")
+                if (uid is None) or (session.get(m_users, uid) is None):
                     return {"message": f"User {uid} not found"}, 404
                 submission.uid = uid
 
                 # Project
-                project = session.get(m_projects, pid)
-                if project is None:
-                    return {"message": f"Project {pid} not found"}, 404
-                submission.project_id = pid
+                project_id = request.form.get("project_id")
+                if (project_id is None) or (session.get(m_projects, project_id)):
+                    return {"message": f"Project {project_id} not found"}, 404
+                submission.project_id = project_id
 
                 # Grading
                 if "grading" in request.form:
@@ -93,11 +89,12 @@ class Submissions(Resource):
 
                 session.add(submission)
                 session.commit()
-                return {"submission": f"/submissions/{submission.submission_id}"}, 201
+                return {
+                    "submission": f"{getenv('HOSTNAME')}/submissions/{submission.submission_id}"
+                }, 201
         except exc.SQLAlchemyError:
             session.rollback()
-            return {"message": f"An error occurred while creating a new submission "
-                f"for user {uid} in project {pid}"}, 500
+            return {"message": "An error occurred while creating a new submission "}, 500
 
 class Submission(Resource):
     """API endpoint for the submission"""
@@ -189,9 +186,5 @@ class Submission(Resource):
             db.session.rollback()
             return {"message": f"An error occurred while deleting submission {sid}"}, 500
 
-submissions_bp.add_url_rule(
-    "/submissions/<string:uid>/<int:pid>",
-    view_func=Submissions.as_view("submissions"))
-submissions_bp.add_url_rule(
-    "/submissions/<int:sid>",
-    view_func=Submission.as_view("submission"))
+submissions_bp.add_url_rule("/submissions", view_func=Submissions.as_view("submissions"))
+submissions_bp.add_url_rule("/submissions/<int:sid>", view_func=Submission.as_view("submission"))
