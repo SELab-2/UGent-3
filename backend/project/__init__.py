1cf7dacf4296f513047127146bb706a46c5c6de3
warre <warre.provoost@ugent.be>
2024-03-02 16:13:06 +0100

    merged with dev

--- backend/project/__init__.py
+++ backend/project/__init__.py
@@ -5,6 +5,7 @@ This file is the base of the Flask API. It contains the basic structure of the A
 from flask import Flask
 from .db_in import db
 from .endpoints.index.index import index_bp
+from .endpoints.users import users_bp
 
 
 def create_app():
@@ -16,6 +17,7 @@ def create_app():
 
     app = Flask(__name__)
     app.register_blueprint(index_bp)
+    app.register_blueprint(users_bp)
 
     return app
 
