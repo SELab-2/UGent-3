4c6a99457cc60537d9cb8cb00d78a002a714a944
cmekeirl <cedric.mekeirle@ugent.be>
2024-03-06 13:32:28 +0100

    base for detail course pag

--- frontend/src/routes/courses.tsx
+++ frontend/src/routes/courses.tsx
@@ -1,8 +1,17 @@
 import { useState, useEffect } from 'react';
-import { Container, Typography, Box, Button, Grid, Card, CardContent } from "@mui/material";
+import { Container, Typography, Grid, Card, CardContent } from "@mui/material";
+import { useParams } from 'react-router-dom';
+
+interface Course{
+    course_id: number,
+    name: string,
+    teacher:string,
+    ufora_id:string,
+    url:string
+}
 
 export default function All_courses() {
-    const [courses, setCourses] = useState<string[]>([]);
+    const [courses, setCourses] = useState<Course[]>([]);
 
     useEffect(() => {
         fetch("http://127.0.0.1:5000/courses")
@@ -17,20 +26,39 @@ export default function All_courses() {
                 <Typography variant="h1" display="flex" justifyContent="center">Courses</Typography>
                 <Typography variant= "h2" display="flex" justifyContent="center">Please select a course for more details</Typography>
                 <Grid container spacing={3}>
-                    {courses.map((courseUrl, index) => (
-                        <Grid item xs={12} sm={6} md={4} key={index}>
-                            <Card>
+                    {courses.map((course) => (
+                        <Grid item xs={12} sm={6} md={4} key={course.course_id}>
+                            <Card onClick={() => {
+                                window.location.href += "/"+course.course_id;
+                            }} style={{cursor: 'pointer', backgroundColor: 'gray'}}>
                                 <CardContent>
-                                    <Button variant="contained" size="large" fullWidth onClick={() => {
-                                        window.location.href = courseUrl; // navigate to the course URL when clicked
-                                    }}>
-                                        {courseUrl}
-                                    </Button>
+                                    <Typography variant="h5">Name: {course.name}</Typography>
+                                    <Typography variant="h6">Ufora id: {course.ufora_id}</Typography>
                                 </CardContent>
                             </Card>
                         </Grid>
                     ))}
                 </Grid>
+            </Container>
+        </div>
+    );
+}
+
+export function Details_course() {
+    const { courseId } = useParams<{ courseId: string }>();
+    const [course, setCourse] = useState<Course>();
+
+    useEffect(() => {
+        fetch("http://127.0.0.1:5000/courses/"+courseId)
+            .then(response => response.json())
+            .then(data => setCourse(data.data))
+            .catch(error => console.error('Error:', error));
+    }, []);
+    return (
+        <div>
+            <Container>
+                <Typography variant="h1" display="flex" justifyContent="center">Course details</Typography>
+                
             </Container>
         </div>
     );
