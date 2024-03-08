65cb9b8e724010cea5dfff8d9dcb6e477e20f210
cmekeirl <cedric.mekeirle@ugent.be>
2024-03-08 09:10:19 +0100

    route fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
         path: "courses",
         children: [
           {
-            path: "/",
+            path: "",
             element: <All_courses />
           },
           {
