27bf31b0e7b22e160f0eb4add5f844f0e80850b6
cmekeirl <cedric.mekeirle@ugent.be>
2024-03-11 12:43:44 +0100

    deprecated material-ui

--- frontend/src/components/Header/Navbar.tsx
+++ frontend/src/components/Header/Navbar.tsx
@@ -3,47 +3,25 @@ import {
   IconButton,
   List,
   ListItemText,
-  makeStyles,
   CssBaseline,
   Drawer,
-  Grid
-} from "@material-ui/core";
-import { ListItemButton } from '@mui/material';
+  Grid,
+  ListItemButton,
+} from "@mui/material";
 import {
   Menu
-} from "@material-ui/icons";
+} from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-
-const useStyles = makeStyles((theme) => ({
-  menuSliderContainer: {
-    width: 250,
-    background: "#373b42",
-    height: "100%"
-  },
-  avatar: {
-    margin: "0.5rem auto",
-    padding: "1rem",
-    width: theme.spacing(13),
-    height: theme.spacing(13)
-  },
-  listItem: {
-    color: "white"
-  },
-  iconButton: {
-    color: "blue"
-  }
-}));
 
 /**
  * Renders the navbar.
  * @returns - A Drawer with a list of navigation items.
  */
 export default function Navbar() {
-  const classes = useStyles();
   const [open, setOpen] = useState(false);
   const navigate = useNavigate();
-  const toggleSlider = (openState: boolean) => () => {
+  const toggleSlider = (openState:boolean) => () => {
     setOpen(openState);
   };
   const { t } = useTranslation();
@@ -54,9 +32,15 @@ export default function Navbar() {
   ]
 
   const SideList = () => (
-    <Grid container direction="column" className={classes.menuSliderContainer}>
+    <Grid container direction="column" sx={{
+      width: 250,
+      background: "#373b42",
+      height: "100%"
+    }}>
       <Grid item container direction="row" alignItems="flex-start">
-        <IconButton onClick={toggleSlider(!open)} className={classes.iconButton}>
+        <IconButton onClick={toggleSlider(!open)} sx={{
+          color: "blue"
+        }}>
           <Menu style={{fontSize:"3rem"}} />
         </IconButton>
       </Grid>
@@ -68,7 +52,7 @@ export default function Navbar() {
             <ListItemButton key={index}
               onClick={() => {return navigate(listItem.link)}}
             >
-              <ListItemText primary={listItem.text} className={classes.listItem} />
+              <ListItemText primary={listItem.text} sx={{color:"white"}} />
             </ListItemButton>
           ))}
         </List>
@@ -79,7 +63,7 @@ export default function Navbar() {
   return (
     <>
       <CssBaseline />
-      <IconButton className={classes.iconButton} onClick={toggleSlider(!open)} style={{ position: 'absolute', top: 0, left: 0 }}>
+      <IconButton sx={{color:"blue"}} onClick={toggleSlider(!open)} style={{ position: 'absolute', top: 0, left: 0 }}>
         <Menu style={{fontSize:"3rem"}} />
       </IconButton>
       <Drawer open={open} anchor="left" onClose={toggleSlider(false)}>
