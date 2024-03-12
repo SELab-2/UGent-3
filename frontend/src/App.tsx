b474f58298ceb9f50d2a5050d11fff590c6a0e2c
cmekeirl <cedric.mekeirle@ugent.be>
2024-03-12 16:43:47 +0100

    changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter,Route,Routes } from "react-router-dom";
 import { Header } from "./components/Header/Header";
-
+import Home from "./pages/home/Home";
 /**
  * This component is the main application component that will be rendered by the ReactDOM. 
  * @returns - The main application component
@@ -9,6 +9,9 @@ function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Header />
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,32 +1,94 @@
-import {
-  AppBar,
-  Box,
-  Button,
-  Toolbar,
-  Typography
-} from "@mui/material";
-import Navbar from "./Navbar";
+import { AppBar, Box, Button, Drawer, Grid, IconButton, List, ListItemButton, ListItemText, Toolbar, Typography } from "@mui/material";
+import { Menu } from "@mui/icons-material";
 import { useTranslation } from 'react-i18next';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useState } from "react";
+
 /**
- * The header component for the application that will be rendered at the top of the page.
- * @returns - The header component
+ * Renders the header component.
+ * @returns JSX.Element representing the header.
  */
 export function Header(): JSX.Element {
   const { t } = useTranslation();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+
+  const listItems = [
+    {link: "/", text: t("homepage")},
+    {link: "/projects", text: t("myProjects")},
+    {link: "/courses", text: t("myCourses")}
+  ]
+
+  const title = getTitle(location.pathname, t);
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="sticky">
-        <Toolbar>
-          <Box sx={{ flexGrow: 1 }}>
-            <Navbar />
-          </Box>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            {t('home')}
+        <Toolbar disableGutters>
+          <IconButton edge="start" onClick={() => setOpen(!open)} sx={{ color: "white", marginLeft: 0 }}>
+            <Menu style={{fontSize:"2rem"}} />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 0 }}>
+            {title}
           </Typography>
+          <Box sx={{ flexGrow: 1 }} />
           <Button color="inherit">{t('login')}</Button>
         </Toolbar>
       </AppBar>
+      <DrawerMenu open={open} onClose={() => setOpen(false)} listItems={listItems} navigate={navigate} />
     </Box>
+  );
+}
+
+/**
+ * Get the title based on the given pathname.
+ * @param pathname - The current pathname.
+ * @param t - The translation function.
+ * @returns The title.
+ */
+function getTitle(pathname: string, t: (key: string) => string): string {
+  switch(pathname) {
+  case '/': return t('home');
+  case '/login': return t('login');
+  case '/courses': return t('myCourses');
+  case '/projects': return t('myProjects');
+  default: return t('home');
+  }
+}
+
+// eslint-disable-next-line jsdoc/require-param
+/**
+ * Renders the drawer menu component.
+ * @param open - Whether the drawer menu is open or not.
+ * @param onClose - Function to handle the close event of the drawer menu.
+ * @param listItems - Array of objects representing the list items in the drawer menu.
+ * @returns - The Side Bar
+ */
+function DrawerMenu({ open, onClose, listItems }: { open: boolean, onClose: () => void, listItems: { link: string, text: string }[] }) {
+
+  return (
+    <Drawer open={open} anchor="left" onClose={onClose}>
+      <Grid container direction="column" sx={{
+        width: 250,
+        height: "100%",
+        backgroundColor: "primary.main"
+      }}>
+        <Grid item container direction="row" alignItems="flex-start">
+          <IconButton onClick={onClose} sx={{
+            color: "white"
+          }}>
+            <Menu style={{fontSize:"2rem"}} />
+          </IconButton>
+        </Grid>
+        <List>
+          {listItems.map((listItem, index) => (
+            <ListItemButton key={index} component={Link} to={listItem.link} role="listitem">
+              <ListItemText primary={listItem.text} sx={{color:"white"}} />
+            </ListItemButton>
+          ))}
+        </List>
+      </Grid>
+    </Drawer>
   );
 }
