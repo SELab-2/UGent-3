ab63ab02f0926a9646e8acc27f970c490aed4160
Siebe Vlietinck <siebe.vlietinck@ugent.be>
2024-03-13 17:22:01 +0100

    poging testen

--- backend/test_auth_server/__main__.py
+++ backend/test_auth_server/__main__.py
@@ -1,10 +1,48 @@
 """Main entry point for the application."""
 
 from dotenv import load_dotenv
-from test_auth_server import create_app_with_db
-from test_auth_server.db_in import url
+from flask import Flask
+
+"""Index api point"""
+from flask import Blueprint, request
+from flask_restful import Resource, Api
+
+index_bp = Blueprint("index", __name__)
+index_endpoint = Api(index_bp)
+
+token_dict = {
+    "teacher1":{
+        "id":"brinkmann",
+        "jobCategory":"teacher"
+    },
+    "teacher2":{
+        "id":"laermans",
+        "jobCategory":"teacher"
+    }
+}
+
+class Index(Resource):
+    """Api endpoint for the / route"""
+
+    def get(self):
+        auth = request.headers.get("Authorization")
+        if not auth:
+            return {"message":"Please give authorization"}, 401
+        bearer, token = auth.split(" ")
+        if bearer != "Bearer":
+            return {"message":"Not this kind of authorization"}, 401
+        if token in token_dict.keys():
+            return token_dict[token], 200
+        return {"message":"Wrong address"}, 401
+        
+
+index_bp.add_url_rule("/", view_func=Index.as_view("index"))
 
 if __name__ == "__main__":
     load_dotenv()
-    app = create_app_with_db(url)
+
+    app = Flask(__name__)
+    app.register_blueprint(index_bp)
+
     app.run(debug=True, host='0.0.0.0')
+
--- backend/test_auth_server/db_construct.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE user_data (
-    access_token VARCHAR(255),
-	id VARCHAR(255),
-	jobCategory VARCHAR(255),
-	PRIMARY KEY(access_token)
-);
