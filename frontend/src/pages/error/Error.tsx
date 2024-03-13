51f824099b10b1a23a8bedf35140630b571ac240
matisse <matisse.sulzer@ugent.be>
2024-03-13 19:13:30 +0100

    indentation fixed

--- frontend/src/pages/error/Error.tsx
+++ frontend/src/pages/error/Error.tsx
@@ -1,32 +1,32 @@
 import { isRouteErrorResponse, useRouteError } from "react-router-dom"
 
 export function Error() {
-    const error = useRouteError();
+  const error = useRouteError();
 
-    if (isRouteErrorResponse(error)) {
+  if (isRouteErrorResponse(error)) {
 
-        let message = "";
-        if (error.status == 403) {
-            message = "Forbidden"
-        } else if (error.status == 404) {
-            message = "Page not found";
-        } else if (error.status == 500) {
-            message = "Internal server error";
-        } else if (error.status == 503) {
-            message = "Looks like our API is down";
-        } else if (500 <= error.status && error.status <= 599) {
-            message = "General server error";
-        }
-
-        return(
-            <div>
-                <p>
-                    { error.status }
-                </p>
-                <p>
-                    { message }
-                </p>
-            </div>
-        )
+    let message = "";
+    if (error.status == 403) {
+      message = "Forbidden"
+    } else if (error.status == 404) {
+      message = "Page not found";
+    } else if (error.status == 500) {
+      message = "Internal server error";
+    } else if (error.status == 503) {
+      message = "Looks like our API is down";
+    } else if (500 <= error.status && error.status <= 599) {
+      message = "General server error";
     }
+
+    return(
+      <div>
+        <h1>
+          { error.status }
+        </h1>
+        <p>
+          { message }
+        </p>
+      </div>
+    )
+  }
 }
