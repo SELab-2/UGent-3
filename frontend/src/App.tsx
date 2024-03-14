983c7215a37b18b82b88f304ae755b66fe43e3b4
matisse <matisse.sulzer@ugent.be>
2024-03-14 17:42:31 +0100

    error pages v1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import { Header } from "./components/Header/Header";
+import { NotFound } from "./pages/error/NotFound";
 
 /**
  * This component is the main application component that will be rendered by the ReactDOM. 
@@ -12,6 +13,7 @@ function App(): JSX.Element {
       <Header />
       <Routes>
         <Route index element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
