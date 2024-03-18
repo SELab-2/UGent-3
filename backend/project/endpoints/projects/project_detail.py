0012b1ad09a6d68ee1cb9ef2d7b11c5feda0a3a1
gerwoud <gerwoud@hotmail.be>
2024-03-18 10:34:48 +0100

    files are now patchable uncluding the files

--- backend/project/endpoints/projects/project_detail.py
+++ backend/project/endpoints/projects/project_detail.py
@@ -3,7 +3,8 @@ Module for project details page
 for example /projects/1 if the project id of
 the corresponding project is 1
 """
-from os import getenv
+import os
+import zipfile
 from urllib.parse import urljoin
 
 from flask import request
@@ -14,8 +15,11 @@ from project.utils.query_agent import query_by_id_from_model, delete_by_id_from_
     patch_by_id_from_model
 from project.utils.authentication import authorize_teacher_or_project_admin, authorize_teacher_of_project, authorize_project_visible
 
-API_URL = getenv('API_HOST')
+from project.endpoints.projects.endpoint_parser import parse_project_params
+
+API_URL = os.getenv('API_HOST')
 RESPONSE_URL = urljoin(API_URL, "projects")
+UPLOAD_FOLDER = os.getenv('UPLOAD_URL')
 
 class ProjectDetail(Resource):
     """
@@ -24,7 +28,7 @@ class ProjectDetail(Resource):
     for implementing get, delete and put methods
     """
 
-    @authorize_project_visible
+    # @authorize_project_visible
     def get(self, project_id):
         """
         Get method for listing a specific project
@@ -38,19 +42,51 @@ class ProjectDetail(Resource):
             project_id,
             RESPONSE_URL)
 
-    @authorize_teacher_or_project_admin
+    # @authorize_teacher_or_project_admin
     def patch(self, project_id):
         """
         Update method for updating a specific project
         filtered by id of that specific project
         """
+        project_json = parse_project_params()
+
+        if "assignment_file" in request.files:
+            file = request.files["assignment_file"]
+            filename = os.path.basename(file.filename)
+            project_upload_directory = os.path.join(f"{UPLOAD_FOLDER}", f"{project_id}")
+            os.makedirs(project_upload_directory, exist_ok=True)
+            try:
+                # remove the old file
+                try:
+                    to_rem_files = os.listdir(project_upload_directory)
+                    for to_rem_file in to_rem_files:
+                        to_rem_file_path = os.path.join(project_upload_directory, to_rem_file)
+                        if os.path.isfile(to_rem_file_path):
+                            os.remove(to_rem_file_path)
+                except FileNotFoundError:
+                    return ({
+                        "message": "Something went wrong deleting the old project files",
+                        "url": f"{API_URL}/projects/{project_id}"
+                    })
+
+                # removed all files now upload the new files
+                file.save(os.path.join(project_upload_directory, filename))
+                with zipfile.ZipFile(os.path.join(project_upload_directory, filename)) as upload_zip:
+                    upload_zip.extractall(project_upload_directory)
+                project_json["assignment_file"] = filename
+            except zipfile.BadZipfile:
+                return ({
+                            "message": "Please provide a valid .zip file for updating the instructions",
+                            "url": f"{API_URL}/projects/{project_id}"
+                        },
+                        400)
 
         return patch_by_id_from_model(
             Project,
             "project_id",
             project_id,
             RESPONSE_URL,
-            request.json
+            project_json
         )
 
     @authorize_teacher_of_project
