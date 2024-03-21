a41c8ea3e8705ff3e95d8282bcae04713fec02d9
gerwoud <gerwoud@hotmail.be>
2024-03-21 15:21:12 +0100

    linter+test succeed

--- backend/project/endpoints/docs/docs_endpoint.py
+++ backend/project/endpoints/docs/docs_endpoint.py
@@ -2,10 +2,11 @@
 Module for defining the swagger docs
 """
 
+from os import getenv
 from flask_swagger_ui import get_swaggerui_blueprint
 
-SWAGGER_URL = "/docs"
-API_URL = "/static/testyallah.json"
+SWAGGER_URL = getenv("DOCS_URL")
+API_URL = getenv("DOCS_JSON_PATH")
 
 swagger_ui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
