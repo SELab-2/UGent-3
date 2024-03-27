673a1403d930397adc8076ff5f1bc8a3b7d723e3
cmekeirl <cedric.mekeirle@ugent.be>
2024-03-27 14:17:07 +0100

    basic

--- frontend/src/components/Courses/Teacher.tsx
+++ frontend/src/components/Courses/Teacher.tsx
@@ -15,10 +15,17 @@ interface Project{
     project_id: number
 }
 
+interface UserUid{
+  uid: string
+}
+
 export function CourseDetailTeacher(): JSX.Element {
   const [course, setCourse] = useState<Course>();
   const [projects, setProjects] = useState<Project[]>([]);
+  const [admins, setAdmins] = useState<UserUid[]>([]);
+  const [students, setStudents] = useState<UserUid[]>([]);
   const { courseId } = useParams<{ courseId: string }>();
+
   useEffect(() => {
     fetch("http://127.0.0.1:5000/courses/" + courseId, {
       headers: {
@@ -44,6 +51,30 @@ export function CourseDetailTeacher(): JSX.Element {
     }
   }, [courseId]);
   
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/courses/" + courseId + "/admins", {
+      headers: {
+        "Authorization": "teacher1"
+      }
+    })
+      .then(response => response.json())
+      .then(data => setAdmins(data.data))
+      .catch(error => console.error('Error:', error));
+
+  }, [courseId]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000//courses/" + courseId + "/students", {
+      headers: {
+        "Authorization": "teacher1"
+      }
+    })
+      .then(response => response.json())
+      .then(data => setStudents(data.data))
+      .catch(error => console.error('Error:', error));
+
+  }, [courseId]);
+  
   return (
     <Grid container direction={'row'} spacing={2}>
       <Grid item>
@@ -55,7 +86,29 @@ export function CourseDetailTeacher(): JSX.Element {
               <Typography variant="body1"><Link to={'/projects/'+project.project_id}>{project.title}</Link></Typography>
             </Grid>
           ))}
-          <Grid item><Button>New Project</Button></Grid>
+          <Grid item><Button><Link to={'/projects/create?courseId='+courseId}>New Project</Link></Button></Grid>
+        </Grid>
+        <Grid container direction={'column'} spacing={2}>
+          <Grid item><Typography variant="h6">lijst co-lesgevers/assistenten</Typography></Grid>
+          {
+            admins.map((admin) => (
+              <Grid item key={admin.uid}>
+                <Typography variant="body1">{admin.uid}</Typography>
+              </Grid>
+            ))
+          }
+          <Grid item><Button>nieuwe lesgever</Button></Grid>
+        </Grid>
+        <Grid container direction={'column'} spacing={2}>
+          <Grid item><Typography variant="h6">lijst studenten</Typography></Grid>
+          {
+            students.map((student) => (
+              <Grid item key={student.uid}>
+                <Typography variant="body1">{student.uid}</Typography>
+              </Grid>
+            ))
+          }
+          <Grid item><Button>nieuwe student(en)</Button></Grid>
         </Grid>
       </Grid>
     </Grid>
