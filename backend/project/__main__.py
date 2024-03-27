0f55af4c51190020cbf129fdc93de047a9a8d74e
cmekeirl <cedric.mekeirle@ugent.be>
2024-03-27 14:19:09 +0100

    allow cors

--- backend/project/__main__.py
+++ backend/project/__main__.py
@@ -2,6 +2,7 @@
 
 from os import getenv
 from dotenv import load_dotenv
+from flask_cors import CORS
 from project import create_app_with_db
 from project.db_in import url
 
@@ -10,7 +11,7 @@ DEBUG=getenv("DEBUG")
 
 if __name__ == "__main__":
     app = create_app_with_db(url)
-
+    CORS(app)
     if DEBUG and DEBUG.lower() == "true":
         app.run(debug=True, host='0.0.0.0')
     else:
