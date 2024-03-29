dd325e700a03c10a8c0510eb1032ed29de9e3868
cmekeirl <cedric.mekeirle@ugent.be>
2024-03-29 10:40:04 +0100

    using the right components

--- frontend/src/components/Courses/CourseForm.tsx
+++ frontend/src/components/Courses/CourseForm.tsx
@@ -1,5 +1,6 @@
+import React from 'react';
 import { useState } from 'react';
-import { Grid, TextField, Button, Typography} from '@mui/material';
+import { FormControl, Box, TextField, Button} from '@mui/material';
 
 /**
  * Renders the CourseForm component.
@@ -7,45 +8,60 @@ import { Grid, TextField, Button, Typography} from '@mui/material';
  */
 export function CourseForm(): JSX.Element {
   const [courseName, setCourseName] = useState('');
+  const apiHost = import.meta.env.VITE_API_HOST
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCourseName(event.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // Prevents the default form submission behaviour
+    // Process and send formData to the server or perform other actions
+    console.log('Course name submitted:', courseName);
+    const data = {name: courseName};
+    call_to_api(apiHost + '/courses', JSON.stringify(data) , 'POST');
+  };
 
   return (
-    <Grid container direction={'column'} spacing={2}>
-      <Grid item>
-        <Grid container direction={'column'} spacing={2} justifyContent="center" alignItems="center" style={{ minHeight: '80vh', maxWidth: '100%' }} >
-          <Grid item>
-            <Typography variant="h4">Naam Vak</Typography>
-          </Grid>
-          <Grid item>
-            <TextField
-              value={courseName}
-              onChange={(e) => setCourseName(e.target.value)}
-              sx={{ width: '50ch', maxWidth: '100%'}}
-            />
-          </Grid>
-        </Grid>
-      </Grid>
-      <Grid item container justifyContent="flex-end">
-        <Button variant="contained" color="primary" style={{ borderRadius: '50px',paddingLeft:'2rem',paddingRight:'2rem' }}
-          onClick={() => {
-            fetch('http://127.0.0.1:5000/courses', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'teacher1'
-              },
-              body: JSON.stringify({ name: courseName }),
-            })
-              .then(response => response.json())
-              .then(data => {
-                window.location.href = data.url; // navigate to data.url
-              })
-              .catch((error) => {
-                console.error('Error:', error); //should redirect to error page
-              });
-          }}>
-          <Typography variant='h5'>Opslaan</Typography>
+    <Box display="flex" justifyContent="center" alignItems="center" height="90vh" position="relative">
+      <form onSubmit={handleSubmit}>
+        <FormControl>
+          <TextField label="course name" value={courseName} onChange={handleInputChange} />
+        </FormControl>
+        <Button
+          type="submit"
+          style={{
+            position: 'absolute',
+            bottom: '15rem',
+            right: '30rem',
+          }}
+        >Submit
         </Button>
-      </Grid>
-    </Grid>
+      </form>
+    </Box>
   );
+}
+
+/**
+ * Helper function to send requests to the a</Button>pi
+ * @param path - path to the api 
+ * @param data - data to send
+ * @param method - POST, PATCH, GET or DELETE
+ */
+function call_to_api(path: string, data: string, method:string){
+  fetch(path, {
+    method: method,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'teacher1'
+    },
+    body: data,
+  })
+    .then(response => response.json())
+    .then(data => {
+      window.location.href = data.url; // navigate to data.url
+    })
+    .catch((error) => {
+      console.error('Error:', error); //should redirect to error page
+    });
 }
