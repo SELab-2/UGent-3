19bf54093dbaefae4d7e8aa57f60e7922943c135
abuzogan <aron.buzogany@ugent.be>
2024-03-31 15:38:40 +0200

    configured language routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import { Header } from "./components/Header/Header";
+import LanguagePath from "./components/LanguagePath";
 
 /**
  * This component is the main application component that will be rendered by the ReactDOM. 
@@ -12,6 +13,9 @@ function App(): JSX.Element {
       <Header />
       <Routes>
         <Route index element={<Home />} />
+        <Route path=":lang" element={<LanguagePath/>}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
