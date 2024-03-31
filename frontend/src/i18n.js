fe20e9fc800de4d5d30d5b21743c9579705255af
abuzogan <aron.buzogany@ugent.be>
2024-03-31 15:38:15 +0200

    configured detector to detect language in path

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -3,6 +3,11 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+const detectionOptions = {
+  order: ['path', 'navigator', 'localStorage', 'subdomain', 'queryString', 'htmlTag'],
+  lookupFromPathIndex: 0
+}
+
 i18n
   .use(Backend)
   .use(LanguageDetector)
@@ -10,7 +15,7 @@ i18n
   .init({
     fallbackLng: 'en',
     debug: true,
-
+    detection: detectionOptions,
     interpolation: {
       escapeValue: false,
     }
