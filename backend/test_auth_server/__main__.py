26c4804c9446886709c34d88b4e76d7e08955829
gerwoud <gerwoud@hotmail.be>
2024-04-03 18:47:04 +0200

    backend linter

--- backend/test_auth_server/__main__.py
+++ backend/test_auth_server/__main__.py
@@ -1,10 +1,7 @@
 """Main entry point for the application."""
 
 from dotenv import load_dotenv
-from flask import Flask
-
-"""Index api point"""
-from flask import Blueprint, request
+from flask import Flask, Blueprint, request
 from flask_restful import Resource, Api
 
 
