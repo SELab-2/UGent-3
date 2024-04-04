fbef6996cce1d150d98bc553440a541b299220f8
cmekeirl <cedric.mekeirle@ugent.be>
2024-04-04 20:22:41 +0200

    Set title using Portal

---
+++ frontend/src/components/Header/PageTitle.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+export const PageTitle = ({ title, defaultTitle, className }: {title:string,defaultTitle:string,className:string}) => {
+
+  return (
+    <span className={className}>
+      {!title ? (
+        <span>{defaultTitle}</span>
+      ) : typeof title === 'string' ? (
+        <span>{title}</span>
+      ) : (
+        title
+      )}
+    </span>
+  );
+};
