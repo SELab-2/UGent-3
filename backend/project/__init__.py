3c28a9b3ec9bf8e7e8067feff39bdd871a57f350
Siebe Vlietinck <siebe.vlietinck@ugent.be>
2024-04-16 22:36:50 +0200

    added: implicitly refresh access tokens

--- backend/project/__init__.py
+++ backend/project/__init__.py
@@ -2,12 +2,13 @@
 This file is the base of the Flask API. It contains the basic structure of the API.    
 """
 from os import getenv
-from datetime import timedelta
+from datetime import timedelta, timezone, datetime
 
 from dotenv import load_dotenv
 
 from flask import Flask
-from flask_jwt_extended import JWTManager
+from flask_jwt_extended import JWTManager, get_jwt, get_jwt_identity,\
+      create_access_token, set_access_cookies
 from flask_cors import CORS
 from .db_in import db
 from .endpoints.index.index import index_bp
@@ -47,7 +48,7 @@ def create_app():
     app.register_blueprint(auth_bp)
 
     jwt = JWTManager(app)
-    auth_init(jwt)
+    auth_init(jwt, app)
     return app
 
 def create_app_with_db(db_uri: str):
@@ -67,10 +68,13 @@ def create_app_with_db(db_uri: str):
     CORS(app, supports_credentials=True)
     return app
 
-def auth_init(jwt):
+
+
+def auth_init(jwt, app):
     """
     This function will overwrite the default return messages from
     the flask-jwt-extended package with custom messages
+    and make it so the access tokens implicitly refresh
     """
     @jwt.expired_token_loader
     def expired_token_callback(jwt_header, jwt_payload):
@@ -95,3 +99,22 @@ def auth_init(jwt):
     @jwt.unauthorized_loader
     def unauthorized_callback(jwt_header):
         return {"message":"You need an access token to get this data, please log in"}, 401
+
+    @app.after_request
+    def refresh_expiring_jwts(response):
+        try:
+            exp_timestamp = get_jwt()["exp"]
+            now = datetime.now(timezone.utc)
+            target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+            if target_timestamp > exp_timestamp:
+                access_token = create_access_token(
+                    identity=get_jwt_identity(),
+                    additional_claims=
+                        {"is_admin":get_jwt()["is_admin"],
+                        "is_teacher":get_jwt()["is_teacher"]}
+                    )
+                set_access_cookies(response, access_token)
+            return response
+        except (RuntimeError, KeyError):
+            # Case where there is not a valid JWT. Just return the original response
+            return response
