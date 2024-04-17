b4f46f39f90b110163ff817e507e2aec339cc693
warre <warre.provoost@ugent.be>
2024-04-17 19:05:41 +0200

    homepage change

--- frontend/src/pages/home/HomePages.tsx
+++ frontend/src/pages/home/HomePages.tsx
@@ -2,7 +2,10 @@ import HomeStudent from './HomeStudent';
 import Home from "./Home.tsx";
 import Cookies from 'js-cookie';
 
-
+/**
+ * Gives the requested home page based on the login status
+ * @returns - The home page component 
+ */
 export default function HomePages() {
   const loginStatus = Cookies.get('login_status');
 
