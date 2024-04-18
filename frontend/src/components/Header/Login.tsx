cf3bfe71910e993e872595341f7c917144e93638
Siebe Vlietinck <siebe.vlietinck@ugent.be>
2024-04-18 18:14:10 +0200

    fix error + code_challenge in frontend link

--- frontend/src/components/Header/Login.tsx
+++ frontend/src/components/Header/Login.tsx
@@ -1,14 +1,25 @@
-import {Link} from "@mui/material";
+import {Button} from "@mui/material";
+import { Link } from 'react-router-dom';
 
 const CODE_VERIFIER = import.meta.env.VITE_APP_CODE_VERIFIER;
 const CLIENT_ID = import.meta.env.VITE_APP_CLIENT_ID;
 const REDIRECT_URI = encodeURI(import.meta.env.VITE_APP_API_HOST + "/auth");
 const TENANT_ID = import.meta.env.VITE_APP_TENANT_ID;
 
-export function LoginLink(): JSX.Element {
-    const code_challenge = crypto.createHash('sha256')
-        .update(CODE_VERIFIER)
-        .digest('base64');
-    const link = `https://login.microsoftonline.com/${TENANT_ID}/oauth2/v2.0/authorize?prompt=select_account&response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=.default&code_challenge=${code_challenge}&code_challenge_method=S256&state=?`;
-    return (<Link href=`${link}`>Login</Link>)
+async function hash(string: string) {
+    const utf8 = new TextEncoder().encode(string);
+    return crypto.subtle.digest('SHA-256', utf8).then((hashBuffer) => {
+      const hashArray = Array.from(new Uint8Array(hashBuffer));
+      const hashHex = hashArray
+        .map((bytes) => bytes.toString(16).padStart(2, '0'))
+        .join('');
+      return hashHex;
+    });
+  }
+
+export async function LoginLink(): Promise<JSX.Element> {
+    const code_challenge = btoa(await hash(CODE_VERIFIER));
+    const link = `https://login.microsoftonline.com/${TENANT_ID}/oauth2/v2.0/authorize?prompt=select_account&response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=.default&code_challenge=${code_challenge}&code_challenge_method=S256`;
+
+    return <Button variant="contained" component={Link} to={link}>Login</Button>
 };
