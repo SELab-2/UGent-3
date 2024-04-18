5b0840fa5ffa6c15f7db8cf730c6e48ef0471732
Siebe Vlietinck <siebe.vlietinck@ugent.be>
2024-04-18 19:02:41 +0200

    redis added later

--- backend/project/endpoints/authentication/logout.py
+++ backend/project/endpoints/authentication/logout.py
@@ -1,11 +1,9 @@
 """Api endpoint to handle logout requests"""
 from os import getenv
 
-from datetime import timedelta
-
 from dotenv import load_dotenv
 from flask import Blueprint, redirect
-from flask_jwt_extended import unset_jwt_cookies, jwt_required, get_jwt, jwt_redis_blocklist
+from flask_jwt_extended import unset_jwt_cookies, jwt_required
 from flask_restful import Resource, Api
 
 logout_bp = Blueprint("logout", __name__)
@@ -23,10 +21,8 @@ class Logout(Resource):
         Will handle the request according to the method defined in the .env variables.
         Currently only Microsoft and our test authentication are supported
         """
-        jti = get_jwt["jti"]
         resp = redirect(HOMEPAGE_URL, 303)
         unset_jwt_cookies(resp)
-        jwt_redis_blocklist.set(jti, ex=timedelta(hours=3))
         return resp
 
 logout_api.add_resource(Logout, "/logout")
