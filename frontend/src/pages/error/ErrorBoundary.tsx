04c19cbfbe099be973de2ef5cb59bce8cd9b4603
matisse <matisse.sulzer@ugent.be>
2024-04-18 14:39:34 +0200

    use of keyPrefix

--- frontend/src/pages/error/ErrorBoundary.tsx
+++ frontend/src/pages/error/ErrorBoundary.tsx
@@ -9,23 +9,24 @@ import { useTranslation } from "react-i18next";
 export function ErrorBoundary() {
   const error = useRouteError();
   const { t } = useTranslation();
+  const keyPrefix = "error.";
 
   if (isRouteErrorResponse(error)) {
     if (error.status == 404) {
       return (
-        <ErrorPage statusCode={"404"} statusTitle={t("error.pageNotFound")} message={t("error.pageNotFoundMessage")} />
+        <ErrorPage statusCode={"404"} statusTitle={t(keyPrefix + "pageNotFound")} message={t(keyPrefix + "pageNotFoundMessage")} />
       );
     } else if (error.status == 403) {
       return (
-        <ErrorPage statusCode={"403"} statusTitle={t("error.forbidden")} message={t("error.forbiddenMessage")} />
+        <ErrorPage statusCode={"403"} statusTitle={t(keyPrefix + "forbidden")} message={t(keyPrefix + "forbiddenMessage")} />
       );
     } else if (error.status >= 400 && error.status <= 499) {
       return (
-        <ErrorPage statusCode={error.statusText} statusTitle={t("error.clientError")} message={t("error.clientErrorMessage")} />
+        <ErrorPage statusCode={error.statusText} statusTitle={t(keyPrefix + "clientError")} message={t(keyPrefix + "clientErrorMessage")} />
       );
     } else if (error.status >= 500 && error.status <= 599) {
       return (
-        <ErrorPage statusCode={error.statusText} statusTitle={t("error.serverError")} message={t("error.serverErrorMessage")} />
+        <ErrorPage statusCode={error.statusText} statusTitle={t(keyPrefix + "serverError")} message={t(keyPrefix + "serverErrorMessage")} />
       );
     }
   }
