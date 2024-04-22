14084135323959e9ed26cc0dc4da6abffb792705
Siebe Vlietinck <siebe.vlietinck@ugent.be>
2024-04-22 15:21:57 +0200

    small fixes

--- frontend/src/utils/csrf.ts
+++ frontend/src/utils/csrf.ts
@@ -1,10 +1,11 @@
+/**
+ * A helper function to easily retrieve the crsf_access_token cookie
+ * @returns the crsf_access_token cookie
+ */
 export function get_csrf_cookie(): string {
-    const cookie = document.cookie
+  const cookie = document.cookie
     .split("; ")
     .find((row) => row.startsWith("csrf_access_token="))
     ?.split("=")[1];
-    if(!cookie) {
-        return "";
-    }
-    return cookie;
+  return cookie ? cookie : "";
 }
