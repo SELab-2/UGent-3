8df96e750b8a7b82ca69d7cb03ac61d14ffd33cd
warre <warre.provoost@ugent.be>
2024-04-23 14:27:50 +0200

    linting

--- frontend/src/utils/fetches/FetchMe.ts
+++ frontend/src/utils/fetches/FetchMe.ts
@@ -2,16 +2,16 @@
 export const fetchMe = async () => {
   const API_URL = import.meta.env.VITE_APP_API_HOST
   try {
-    const response = await fetch(`${API_URL}/me`, {
+    const response = await fetch(`${API_URL}/me`,{
       credentials: 'include'
     })
-    if(response.status == 200){
+    if (response.status == 200) {
       const data  = await response.json()
       return data.data
-    }else {
+    } else {
       return {role:"UNKNOWN"}
     }
-  } catch (e){
+  } catch (e) {
     return {role:"UNKNOWN"}
   }
 }
