8c95085f72407529eb279034d77af2bf744eab83
warre <warre.provoost@ugent.be>
2024-04-27 17:08:56 +0200

    renamed for clarity

--- frontend/src/components/Courses/courses.ts
+++ frontend/src/components/Courses/courses.ts
@@ -2,6 +2,7 @@ import { authenticatedFetch } from "../../utils/authenticated-fetch.ts";
 const API_URL = import.meta.env.VITE_APP_API_HOST;
 
 /**
+ * @param user_uid - the user to fetch the courses from where it is a teacher
  * @returns Courses[]
  */
 export async function  fetchProjectFormCourses(user_uid: string){
