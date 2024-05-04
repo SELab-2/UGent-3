076f0f0024495e567beca5dfbf7aeaddd9624b5c
Aron Buzogany <aronsaps@gmail.com>
2024-05-04 11:47:26 +0200

    Fix #275

--- frontend/src/components/ProjectForm/ProjectForm.tsx
+++ frontend/src/components/ProjectForm/ProjectForm.tsx
@@ -22,7 +22,7 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import DeadlineCalender from "../Calender/DeadlineCalender.tsx";
 import { Deadline } from "../../types/deadline";
 import {InfoOutlined} from "@mui/icons-material";
-import {Link, useLoaderData, useLocation} from "react-router-dom";
+import {Link, redirect, useLoaderData, useLocation, useNavigate} from "react-router-dom";
 import FolderDragDrop from "../FolderUpload/FolderUpload.tsx";
 import TabPanel from "@mui/lab/TabPanel";
 import {TabContext} from "@mui/lab";
@@ -30,6 +30,7 @@ import FileStuctureForm from "./FileStructureForm.tsx";
 import AdvancedRegex from "./AdvancedRegex.tsx";
 import RunnerSelecter from "./RunnerSelecter.tsx";
 import { authenticatedFetch } from "../../utils/authenticated-fetch.ts";
+import i18next from "i18next";
 
 interface Course {
   course_id: string;
@@ -77,12 +78,15 @@ export default function ProjectForm() {
   const [runner, setRunner] = useState<string>('');
   const [validRunner, setValidRunner] = useState(true);
   const [validSubmission, setValidSubmission] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const courses = useLoaderData() as Course[]
 
   const [courseId, setCourseId] = useState<string>('');
   const [courseName, setCourseName] = useState<string>('');
   const location = useLocation();
+
+  const navigate = useNavigate();
 
   useEffect(() =>{
     const urlParams = new URLSearchParams(location.search);
@@ -142,9 +146,15 @@ export default function ProjectForm() {
 
     if (runner === "CUSTOM") {
       setValidRunner(constainsDocker);
+      if (!constainsDocker) {
+        setErrorMessage(t("faultySubmission"));
+      }
       setValidSubmission(constainsDocker);
     } else {
       setValidRunner(containsRuntest);
+      if(!containsRuntest) {
+        setErrorMessage(t("faultySubmission"));
+      }
       setValidSubmission(containsRuntest);
     }
   }
@@ -177,6 +187,7 @@ export default function ProjectForm() {
 
     if (!assignmentFile || !validRunner) {
       setValidSubmission(false);
+      setErrorMessage(t("faultySubmission"));
       return;
     }
 
@@ -212,8 +223,20 @@ export default function ProjectForm() {
     })
 
     if (!response.ok) {
-      throw new Error(t("uploadError"));
+      setValidSubmission(false);
+      if (response.status === 403) {
+        setErrorMessage(t("unauthorized"));
+      }
+      else {
+        setErrorMessage(t("submissionError"));
+      }
+      return;
     }
+
+    response.json().then((data) => {
+      const projectData = data.data;
+      navigate(`/${i18next.language}/projects/${projectData.project_id}`);
+    })
   }
 
   const handleCourseChange = (e: SelectChangeEvent<string>) => {
@@ -422,7 +445,7 @@ export default function ProjectForm() {
             {
               !validSubmission && (
                 <Typography style={{color: 'red', paddingTop: "20px" }}>
-                  {t("faultySubmission")} ⚠️
+                  {errorMessage} ⚠️
                 </Typography>
               )
             }
