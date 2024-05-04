0fc8403e8a5e82516fbfa25d6129273e25490060
Siebe Vlietinck <siebe.vlietinck@ugent.be>
2024-05-04 14:45:13 +0200

    hopefully final version of config files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    jsxInject: 'import React from \'react\'',
+  },
   test: {
-    include: ["**/tests/unit/*-tests.ts?(x)"]
+    include: ["**/tests/unit/*-tests.ts?(x)"],
+    environment: 'jsdom',
+    globals: true,
   },
 })
