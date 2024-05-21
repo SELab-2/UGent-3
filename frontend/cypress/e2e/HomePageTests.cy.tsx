f79d1f39120fda4fe3a22e9d3faaff506dfa4ccc
Siebe Vlietinck <siebe.vlietinck@ugent.be>
2024-05-22 00:12:42 +0200

    added hompage not logged in tests and header login functionality test

---
+++ frontend/cypress/e2e/HomePageTests.cy.tsx
@@ -0,0 +1,15 @@
+describe('Homepage not logged in functionality', () => {
+    it('Header Visible', () => {
+      cy.visit('http://localhost:5173')
+      cy.contains('Peristerónas')
+      cy.get('header').should('be.visible')
+    })
+
+    it('Center button Login', () => {
+        cy.visit('http://localhost:5173')
+        cy.contains('Peristerónas')
+        cy.get('a.MuiButtonBase-root.MuiButton-root.MuiButton-contained.MuiButton-containedPrimary.MuiButton-sizeMedium.MuiButton-containedSizeMedium.MuiButton-colorPrimary.MuiButton-root.MuiButton-contained.MuiButton-containedPrimary.MuiButton-sizeMedium.MuiButton-containedSizeMedium.MuiButton-colorPrimary.css-sghohy-MuiButtonBase-root-MuiButton-root')
+        .eq(1).contains('Login').should('be.visible').should('have.attr', 'href').and('include', 'login.microsoftonline.com')
+      })
+  })
+
