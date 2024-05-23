5f80da2f1f83d91b3988e46acc8951b989ea9581
warre <warre.provoost@ugent.be>
2024-05-23 17:55:45 +0200

    linter

--- frontend/src/components/Courses/CoursesDetail.tsx
+++ frontend/src/components/Courses/CoursesDetail.tsx
@@ -4,6 +4,10 @@ import {Course, ProjectDetail} from "./CourseUtils.tsx";
 import CourseDetailTeacher from "./CourseDetailTeacher.tsx";
 import CourseDetailStudent from "./CourseDetailStudent.tsx";
 
+/**
+ * gives the right detail page
+ * @returns - detail page
+ */
 export default function CoursesDetail() :JSX.Element {
   const loader = useLoaderData() as {
     course: Course;
