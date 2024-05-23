14cffcda18e88fa27468eebf7f7ecc082a7f2ab0
Siebe Vlietinck <siebe.vlietinck@ugent.be>
2024-05-23 21:31:04 +0200

    removed comments

--- frontend/cypress/e2e/Header.cy.tsx
+++ frontend/cypress/e2e/Header.cy.tsx
@@ -1,8 +1,3 @@
-// e2e needs to check that you can actually go from certain page to other pages using navbar/header
-// also logout and maybe changing language? (how to test language stuff?)
-// component tests that everything is present that has to be present
-import me from "../../tests/utils/me-fixture.ts";
-
 describe("Header look not logged in", () => {
   it("App name in header", () => {
     cy.visit("/");
@@ -25,7 +20,6 @@ describe("Header functionality not logged in", () => {
       .should("be.visible")
       .should("have.attr", "href")
       .and("include", "login.microsoftonline.com");
-    // good enough test, will need to test validity of url some other way because can't really login if we can't make our own account for testing
   });
 
   it("Header change language en -> nl", () => {
